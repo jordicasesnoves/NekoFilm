f161680e5102b1903ff99d0b51ca9ae009e900b8
Jordi <jordicasesnoves@gmail.com>
2020-05-08 15:16:32 +0200

    sticky poster and actions on movie page

--- src/components/MediaPageContainer.js
+++ src/components/MediaPageContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const MediaPageContainer = (props) => {
   return (
     <div
-      className="flex py-16 max-w-6xl mx-auto"
+      className="flex py-10 max-w-6xl mx-auto"
       style={{ minHeight: "calc(100vh - 4rem)" }}
     >
       {props.children}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -111,7 +111,23 @@ export const Navbar = () => {
           <div className="px-4 py-2 mr-2 ">
             {state.decodedToken.sub.username}
           </div>
-          <Button onClick={logOut}>Log Out</Button>
+          <Button
+            className="flex items-center justify-center text-center"
+            color="red"
+            onClick={logOut}
+          >
+            Log Out
+            <svg
+              className="w-5 inline ml-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M4,12a1,1,0,0,0,1,1h7.59l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l4-4a1,1,0,0,0,.21-.33,1,1,0,0,0,0-.76,1,1,0,0,0-.21-.33l-4-4a1,1,0,1,0-1.42,1.42L12.59,11H5A1,1,0,0,0,4,12ZM17,2H7A3,3,0,0,0,4,5V8A1,1,0,0,0,6,8V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V16a1,1,0,0,0-2,0v3a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V5A3,3,0,0,0,17,2Z"
+              />
+            </svg>
+          </Button>
         </div>
       </div>
     </div>
