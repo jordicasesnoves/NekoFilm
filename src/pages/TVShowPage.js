d37ee40cad11fd346df8488df7252a0a0fb98c74
Jordi <jordicasesnoves@gmail.com>
2020-05-08 17:46:52 +0200

    fixed TVShowPage trailer

--- src/pages/TVShowPage.js
+++ src/pages/TVShowPage.js
@@ -4,8 +4,14 @@ import { LazyPoster } from "../components/LazyPoster";
 import { CastList } from "../components/CastList";
 import { useMediaData } from "../hooks/useMediaData";
 import { getCountryName } from "../utils/getCountryName";
+import { useModal } from "../hooks/useModal";
+import { Button } from "../components/Button";
+import { TrailerModal } from "../components/TrailerModal";
 
 export const TVShowPage = () => {
+  // Custom hook for displaying the trailer modal
+  const { isShowing, toggle } = useModal();
+
   const [id, trailer, direction, called, loading, data, error] = useMediaData(
     "show"
   );
@@ -16,16 +22,45 @@ export const TVShowPage = () => {
   if (error) return `Error! ${error}`;
 
   return (
-    <>
-      <div className="mr-16 animated fadeIn">
+    <div className="flex">
+      <div className="animated fadeIn sticky top-0 self-start pt-8 mr-16">
         <LazyPoster
           width={300}
-          className="rounded shadow-2xl max-w-none"
+          className="rounded shadow max-w-none mb-4"
           src={data.show.poster_path}
           alt={data.show.name}
         />
+        {trailer.key && (
+          <div style={{ width: "300px" }}>
+            <Button
+              fullWidth
+              className="flex items-center justify-center text-center"
+              color="red"
+              target="_blank"
+              onClick={toggle}
+            >
+              <svg
+                className="w-5 inline mr-3"
+                xmlns="http://www.w3.org/2000/svg"
+                data-name="Layer 1"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M23,9.71a8.5,8.5,0,0,0-.91-4.13,2.92,2.92,0,0,0-1.72-1A78.36,78.36,0,0,0,12,4.27a78.45,78.45,0,0,0-8.34.3,2.87,2.87,0,0,0-1.46.74c-.9.83-1,2.25-1.1,3.45a48.29,48.29,0,0,0,0,6.48,9.55,9.55,0,0,0,.3,2,3.14,3.14,0,0,0,.71,1.36,2.86,2.86,0,0,0,1.49.78,45.18,45.18,0,0,0,6.5.33c3.5.05,6.57,0,10.2-.28a2.88,2.88,0,0,0,1.53-.78,2.49,2.49,0,0,0,.61-1,10.58,10.58,0,0,0,.52-3.4C23,13.69,23,10.31,23,9.71ZM9.74,14.85V8.66l5.92,3.11C14,12.69,11.81,13.73,9.74,14.85Z"
+                />
+              </svg>
+              <span>Watch Trailer</span>
+            </Button>
+          </div>
+        )}
+        <TrailerModal
+          isShowing={isShowing}
+          hide={toggle}
+          trailerURL={trailer.key}
+        />
       </div>
-      <div className="animated fadeIn">
+      <div className="animated fadeIn flex-1 pt-8">
         <div className="flex-row mb-1 items-center">
           <div className="inline mr-2 text-5xl font-medium align-middle">
             {data.show.name}
@@ -73,18 +108,6 @@ export const TVShowPage = () => {
             <span>{data.show.number_of_seasons} </span>
           </li>
           <li>
-            <span className="text-gray-500 font-medium mr-2">CREATED BY </span>
-            <span>
-              {data.show.created_by.length > 0 && (
-                <span className="italic">
-                  {data.show.created_by.map((creator) => (
-                    <span key={creator.id}>{creator.name} </span>
-                  ))}
-                </span>
-              )}
-            </span>
-          </li>
-          <li>
             <span className="text-gray-500 font-medium mr-2">
               LAST AIR DATE{" "}
             </span>
@@ -96,6 +119,19 @@ export const TVShowPage = () => {
             </span>
             <span>{data.show.first_air_date} </span>
           </li>
+          <li>
+            <span className="text-gray-500 font-medium mr-2">CREATED BY </span>
+            <span>
+              {data.show.created_by.length > 0 && (
+                <span className="italic">
+                  {data.show.created_by.map((creator) => (
+                    <span key={creator.id}>{creator.name} </span>
+                  ))}
+                </span>
+              )}
+            </span>
+          </li>
+
           <li>
             <span className="text-gray-500 font-medium mr-2">
               ORIGIN COUNTRIES
@@ -112,6 +148,6 @@ export const TVShowPage = () => {
           </li>
         </ul>
       </div>
-    </>
+    </div>
   );
 };
